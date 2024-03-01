4d32d7129a744be184e4adde965f46585fdf5a23
pixytori28 <victoria.inahuazo11@myhunter.cuny.edu>
2024-03-01 12:46:39 -0500

    Add files via upload

---
+++ h10pseudocode.py
@@ -0,0 +1,12 @@
+#name:Victoria Inahuazo
+#email:victoria.inahuazo11@myhunter.cuny.edu
+#date: 02/29/24
+#the following program prints pseudocode below
+
+import turtle
+t = turtle.Turtle ()
+
+for i in range (5, 301, 5): 
+    print(i)
+    t.forward (i)
+    t.left(91)
